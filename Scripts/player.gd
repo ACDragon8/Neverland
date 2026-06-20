a22d66a3ff6ea9b3774e8a43967bc9119f1a056c
ACDragon <haodeng8@gmail.com>
2026-06-19 17:49:07 -0700

    Initial Commit

---
+++ Scripts/player.gd
@@ -0,0 +1,21 @@
+extends RigidBody2D
+var speed = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("Right"):
+		velocity.x += 1
+	if Input.is_action_pressed("Left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("Jump"):
+		pass
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	position += velocity * delta
